7e7072461905b601141d8ea1c549090c1afbff69
Marco Bettiolo <marco@bettiolo.it>
2014-10-11 04:11:01 +0800

    Testing multiple ips

--- app.js
+++ app.js
@@ -15,26 +15,47 @@ var sdOptions = {
 };
 
 var gnOptions = {
-  host: 'c' + gnClientId.split('-')[0] + '.web.cddbp.net',
+  headers: {
+    host: 'c' + gnClientId.split('-')[0] + '.web.cddbp.net'
+  },
   path: '/webapi/json/1.0/fieldvalues?client=' + gnClientId + '&fieldname=RADIOMOOD&user=' + gnUserId
 };
 
 request('7digital', sdOptions, function() {
-  request('Gracenote', gnOptions);
+  gnOptions.host = '96.46.150.32';
+  request('Gracenote ' + gnOptions.host, gnOptions, function() {
+    gnOptions.host = '208.72.242.176';
+    request('Gracenote ' + gnOptions.host, gnOptions, function() {
+      gnOptions.host = '209.10.42.47';
+      request('Gracenote ' + gnOptions.host, gnOptions);
+    });
+  });
 });
 
 function request(apiName, options, onEndCallback) {
-  console.info('--- Requesting ' + apiName + ' ------------------------------------------------');
-  console.info('URL: https://' + options.host + options.path);
-  console.info(options);
+  console.info('--- Requesting ' + apiName + ' ---');
+  // console.info('URL: https://' + options.host + options.path);
+  // console.info(options);
   var req = https.request(options, function(res) {
     processResponse(res, function() {
-      console.info('--- ' + apiName + ' test completed successfully ------------------------------');
+      console.info('✓✓✓ ' + apiName + ' request successful ✓✓✓');
       console.info('');
       if (onEndCallback) {
         onEndCallback();
       }
     });
+  });
+  req.on('error', function (err) {
+    // console.error(err);
+    console.error('!!! ' + apiName + ' ERROR !!!');
+    console.info('');
+    if (onEndCallback) {
+      onEndCallback();
+    }
+  });
+  req.setTimeout(5000, function(socket){
+    console.error('!!! ' + apiName + ' TIMED OUT !!!');
+    req.abort();
   });
   req.end();
 }
@@ -45,7 +66,7 @@ function processResponse(response, onEnd) {
     str += chunk;
   });
   response.on('end', function () {
-    console.info(str);
+    // console.info(str);
     onEnd();
   });
 }
